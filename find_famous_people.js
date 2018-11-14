69b9e6ce610ce2384c9b8b931b1c2c2da289b37c
froliceric0914 <wzhao.eric@gmail.com>
2018-11-14 02:45:58 +0000

    find famous ppl completed

---
+++ find_famous_people.js
@@ -0,0 +1,37 @@
+const query = process.argv[2];
+console.log(query);
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+const client = new pg.Client({
+  user: settings.user,
+  password: settings.password,
+  database: settings.database,
+  host: settings.hostname,
+  port: settings.port,
+  ssl: settings.ssl
+});
+
+client.connect(err => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  client.query(
+    `SELECT * FROM famous_people WHERE first_name >= \'${query}\'`,
+    (err, result) => {
+      if (err) {
+        return console.error("error running query", err);
+      }
+      console.log(`Found ${result.rows.length} person(s) named ${query}`);
+      let resultStr = "";
+      result.rows.forEach(
+        (obj, pos) =>
+          (resultStr += `${pos + 1}. ${obj.first_name} ${obj.last_name}, born ${
+            obj.birthdate
+          }\n`)
+      );
+      console.log(resultStr);
+      client.end();
+    }
+  );
+});
